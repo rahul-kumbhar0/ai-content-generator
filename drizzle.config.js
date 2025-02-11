6829e460def3c1e0b243505762e07f41f6027aff
rahul kumbhar <rakumbhar5278@gmail.com>
2025-02-11 23:50:45 +0530

    updated files

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,11 @@
 
 
 /** @type { import("drizzle-kit").Config } */
-export default {
+export default {  
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
       url:process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
     }
   };
+  
